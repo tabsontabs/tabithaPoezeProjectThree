f0ce38309ec5c385d835db7ec1e2249209f725b4
tpoeze <tabithapoeze@gmail.com>
2020-06-06 13:57:16 -0400

    added more responsive styling and tried to make JS more programmatic

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const wines = {
-    red: [
+const wines = [
         {
             title: 'Toro Bravo Tempranillo Merlot',
+            color: 'red',
             price: 8.15,
             sweet: 'false',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/635755.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
@@ -9,6 +9,7 @@ const wines = {
         },
         {
             title: "Boone's Sangria",
+            color: 'red',
             price: 7.95,
             sweet: 'true',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/419317.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
@@ -16,6 +17,7 @@ const wines = {
         },
         {
             title: 'Vinologist Cabernet Sauvignon',
+            color: 'red',
             price: 12.95,
             sweet: 'false',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/010416.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
@@ -23,15 +25,15 @@ const wines = {
         },
         {
             title: 'Negrar Appassimento Veneto IGT',
+            color: 'red',
             price: 14.95,
             sweet: 'true',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/629865.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
             description: 'Cantina Valpolicella Negrar has been creating superb wines for 85 years. This red is made by drying grapes for a short period of time (appassimento) resulting in a smooth and rich wine. Full-bodied with aromas and flavours of plum, raisins, herbs, and cherry lingering on the finish. Serve with pizza.'
-        }
-    ],
-    white: [
+        },
         {
             title: 'Sogrape Gazela Vinho Verde',
+            color: 'white',
             price: 9.95,
             sweet: 'true',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/141432.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
@@ -39,6 +41,7 @@ const wines = {
         },
         {
             title: 'Sandbanks Riesling',
+            color: 'white',
             price: 14.95,
             sweet: 'true',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/139022.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
@@ -46,6 +49,7 @@ const wines = {
         },
         {
             title: 'Villa Maria Private Bin Sauvignon Blanc',
+            color: 'white',
             price: 17.95,
             sweet: 'false',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/426601.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
@@ -53,15 +57,15 @@ const wines = {
         },
         {
             title: 'Mission Hill Five Vineyards Sauvignon Blanc',
+            color: 'white',
             price: 9.95,
             sweet: 'false',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/145086.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
             description: 'Clear pale straw colour with aromas of melon, gooseberry and grapefruit; dry with zingy acidity and flavours of tropical fruit followed by a mellow, slightly grassy finish.'
-        }
-    ],
-    rose: [
+        },
         {
             title: 'Lab Rosé Lisboa VR',
+            color: 'rose',
             price: 9.95,
             sweet: 'false',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/632513.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
@@ -69,6 +73,7 @@ const wines = {
         },
         {
             title: 'Barefoot Cellars Pink Pinot Grigio',
+            color: 'rose',
             price: 9.95,
             sweet: 'true',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/639971.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
@@ -76,6 +81,7 @@ const wines = {
         },
         {
             title: 'Flat Rock Pink Twisted Rosé',
+            color: 'rose',
             price: 17.95,
             sweet: 'true',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/039974.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
@@ -83,26 +89,28 @@ const wines = {
         },
         {
             title: 'La Vielle Ferme Rose Ventoux AOC',
+            color: 'rose',
             price: 12.45,
             sweet: 'false',
             url: 'https://www.lcbo.com/content/dam/lcbo/products/622134.jpg/jcr:content/renditions/cq5dam.web.1280.1280.jpeg',
             description: "Grapes grown on the slopes of France's Mont Ventoux ripen slowly at cooler temperatures, maintaining fruit flavours and crisp acidity. This wine is pale rose in colour, with delicate aromas of raspberry and strawberry. Dry and light-bodied, with citrus and raspberry fruit flavours. Serve chilled with tarragon chicken."
         }
-    ]
-};
+];
 
-//document ready
-$(function() {
+const wineRecoApp = {};
 
+//move the user through the form and to their results as they check options
+wineRecoApp.autoScrolling = function() {
     $('header button').click(function () {
         $('html, body').animate({
             scrollTop: $('#q1').offset().top
         });
     })
 
-    $('input[name=color]').click(function(){
+    $('input[name=color]').click(function () {
         $('html, body').animate({
-            scrollTop: $('#q2').offset().top});
+            scrollTop: $('#q2').offset().top
+        });
     })
 
     $('input[name=price]').click(function () {
@@ -127,31 +135,75 @@ $(function() {
         $('html, body').animate({
             scrollTop: $('#q1').offset().top
         });
-        // $('.results').detach();
     })
+}
 
-    $('form').on('submit', function(e){ 
-        //prevent default form behaviour
-        e.preventDefault();
-        //get the values of selected radio buttons
-        const colorPref = $('input[name=color]:checked').val();
-        const pricePref = $('input[name=price]:checked').val();
-        const sugarPref = $('input[name=sugarLevel]:checked').val();
+//filter through the list of wines based on the options the user checked
+wineRecoApp.getWineReco = function() {
 
-        const wineSpecs = wines[colorPref];
+    //when form is submitted...
+    $('form').on('submit', function (e) {
 
-        for(let i = 0; i < wineSpecs.length; i++) {
-            const wineReco = wineSpecs[i];
-            //
-            if (wineReco.price < pricePref && wineReco.sweet == sugarPref) {
-                $('.resultImg').attr('src',`${wineReco.url}`);
-                $('.resultTitle').html(`${wineReco.title}`);
-                $('.resultInfo').html(`${wineReco.description}`);
-                $('.resultPrice').html(`$${wineReco.price}`);
-            }
-        }
+    //prevent default form behaviour
+    e.preventDefault();
 
-        
-    })
+    //get the values of selected radio buttons
+    const colorPref = $('input[name=color]:checked').val();
+    const pricePref = $('input[name=price]:checked').val();
+    const sugarPref = $('input[name=sugarLevel]:checked').val();
 
+    //get an array of wines that match the selected radio buttons
+    const wineReco = wines.filter((value) => {
+        return value.price < pricePref 
+        && value.price > (pricePref - 10)
+        && value.sweet == sugarPref 
+        && value.color == colorPref
+    });
+
+    console.log(wineReco);
+
+    //loop through the array of wines and display a recommendation to the user
+    wineReco.forEach(function (wine) {
+        const userWineImg = $('.resultImg').attr('src', `${wine.url}`);
+        const userWineTitle = $('.resultTitle').text(`${wine.title}`);
+        const userWineInfo = $('.resultInfo').text(`${wine.description}`);
+        const userWinePrice = $('.resultPrice').text(`$${wine.price}`);
+    });
 });
+}
+
+// wineRecoApp.displayWineReco = function (winesArray) {
+    
+// }
+
+    
+    // const wineSpecs = wines[colorPref];
+ 
+
+    // for (let i = 0; i < wineSpecs.length; i++) {
+    //     const wineReco = wineSpecs[i];
+    //     //
+    //     if (wineReco.price < pricePref && wineReco.sweet == sugarPref) {
+    //         $('.resultImg').attr('src', `${wineReco.url}`);
+    //         $('.resultTitle').html(`${wineReco.title}`);
+    //         $('.resultInfo').html(`${wineReco.description}`);
+    //         $('.resultPrice').html(`$${wineReco.price}`);
+    //     }
+    // }
+// }
+// )}
+
+
+
+
+wineRecoApp.init = function() {
+    wineRecoApp.autoScrolling();
+    // wineRecoApp.filterWines();
+    wineRecoApp.getWineReco();
+}
+
+
+//document ready
+$(function() {
+    wineRecoApp.init();
+})
